a238a8b30eb75d931d79c125603bdcdbc02124ef
clau-nunes <claudiarobertapsi@gmail.com>
2022-03-22 15:15:12 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿var path = @"C:\Users\claud\Desktop\dio bootcamp\DemoDiretorio\globo";
+LerDiretorios(path);
+Console.WriteLine("Digite [enter] para finalizar");
+Console.ReadLine();
+static void LerDiretorios(string path)
+{
+    if(Directory.Exists(path))
+    {
+        var diretorios = Directory.GetDirectories(path, "*", SearchOption.AllDirectories);
+        foreach (var dir in diretorios)
+        {
+            var dirInfo = new DirectoryInfo(dir);
+            Console.WriteLine($"[Nome]:{dirInfo.Name}");
+            Console.WriteLine($"[Raiz]:{dirInfo.Root}");
+            if(dirInfo.Parent != null);
+                Console.WriteLine($"[Pai:{dirInfo.Parent.Name}");
+            Console.WriteLine("----------------------");
+
+
+        }
+
+    }
+    else
+    {
+        Console.WriteLine($"{path} não existe" );
+            }
+    
+
+}
